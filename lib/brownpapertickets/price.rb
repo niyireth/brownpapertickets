9d9c61083aa3ba59017c16a6a13c3e69f7112598
Niyireth De la Hoz <niyireth@MacBook-de-Niyireth-De-la-Hoz.local>
2011-02-28 12:30:55 -0500

    Remove puts

--- lib/brownpapertickets/price.rb
+++ lib/brownpapertickets/price.rb
@@ -73,7 +73,6 @@ module BrownPaperTickets
       if self.price_id.blank?
         return false unless validates_required_attr
         new_save("addprice")
-        p "entro en add price"
       else
         new_save("changeprice")
       end
@@ -90,7 +89,6 @@ module BrownPaperTickets
       if param == "addprice"
         self.price_id = (xml/:price_id).inner_html if (xml/:resultcode).inner_html == "000000"
         process_create_response( (xml/:resultcode).inner_html, price_id)
-        p price_id
       else
         process_update_response( (xml/:resultcode).inner_html)
       end
